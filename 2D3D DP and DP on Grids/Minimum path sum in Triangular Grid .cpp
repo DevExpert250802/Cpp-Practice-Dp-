03f6f1b8cb5716650174ec959ad5eba59c074586
Devansh Khandelwal <108886695+dev25082@users.noreply.github.com>
2023-08-31 22:58:20 +0530

    Minimum path sum in Triangular Grid

---
+++ 2D3D DP and DP on Grids/Minimum path sum in Triangular Grid .cpp
@@ -0,0 +1,89 @@
+/* Minimum path sum in Triangular Grid :
+
+
+We are given a Triangular matrix. We need to find the minimum path sum from the first row to the last row.
+
+At every cell we can move in only two directions: either to the bottom cell (↓) or to the bottom-right cell(↘) */
+
+
+
+
+
+
+
+
+
+#include <bits/stdc++.h>
+ int f(int i, int j,vector<vector<int>>&a, int n){
+  if(i==n-1)return a[n-1][j];
+  int down = a[i][j] + f(i+1,j,a,n);
+  int diagonal = a[i][j] + f(i+1,j+1,a,n);
+  return min(down,diagonal);
+}
+int minimumPathSum(vector<vector<int>>&triangle, int n){
+  return f(0,0,triangle,n);
+}
+
+
+
+
+
+
+
+
+#include <bits/stdc++.h>
+ int f(int i, int j,vector<vector<int>>&a, int n,vector<vector<int>>&dp){
+  if(i==n-1)return a[n-1][j];
+  if(dp[i][j]!=-1) return dp[i][j];
+  int down = a[i][j] + f(i+1,j,a,n,dp);
+  int diagonal = a[i][j] + f(i+1,j+1,a,n,dp);
+  return min(down,diagonal);
+}
+int minimumPathSum(vector<vector<int>>&triangle, int n){
+  vector<vector<int>>dp(n,vector<int>(n,-1));
+  return f(0,0,triangle,n,dp);
+}
+
+
+
+
+
+#include <bits/stdc++.h>
+int minimumPathSum(vector<vector<int>>&triangle, int n){
+  vector<vector<int>>dp(n,vector<int>(n,0));
+   for(int j=0;j<n;j++){
+  dp[n-1][j]=triangle[n-1][j];
+}
+int down,diagonal=0;
+for (int i=n-2; i>=0; i--) {
+  for (int j=i; j>=0; j--) {
+    int down = triangle[i][j] + dp[i+1][j];
+    int diagonal = triangle[i][j] + dp[i+1][j+1];
+    dp[i][j] = min(down,diagonal);
+  }
+}
+return dp[0][0];
+}
+
+
+
+
+
+
+#include <bits/stdc++.h>
+int minimumPathSum(vector<vector<int>>&triangle, int n){
+  vector<int>front(n,0) ,curr(n,0);
+   for(int j=0;j<n;j++){
+  front[j]=triangle[n-1][j];
+}
+int down,diagonal=0;
+for (int i=n-2; i>=0; i--) {
+  for (int j=i; j>=0; j--) {
+    int down = triangle[i][j] + front[j];
+    int diagonal = triangle[i][j] + front[j+1];
+    curr[j] = min(down,diagonal);
+  }
+  front = curr;
+}
+return front[0];
+}
