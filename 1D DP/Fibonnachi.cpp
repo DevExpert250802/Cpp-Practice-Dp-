e470f37a7a365d157019e15b3647b006b638c9d2
Devansh Khandelwal <108886695+dev25082@users.noreply.github.com>
2024-01-06 12:47:08 +0530

    Fibonnachi

--- 1D DP/Fibonnachi.cpp
+++ 1D DP/Fibonnachi.cpp
@@ -1,168 +1,3 @@
-# include <iostream>
-using namespace std;
-const int N = 1e5+10;
-
-int dp [N]; // global variable
-
-int fib (int n){
-	if(n<=1) return n;
-		if(dp[n]!=-1) return dp[n];
-	return dp[n] =fib(n-1)+ fib (n-2);
-}
-
-int main(){
-
-memset(dp,-1,sizeof (dp));
-int n ;
-cin >>n;
-cout << fib(n);
-}
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-if(n<=1) return dp[n]=n; 
-
-// As if this DP vector is used later for any purpose
-// dp[0] and dp[1] will print out -1.
-
-///////////////////////////////////////////////////////////////////////////////////////////
-
-// error - dp not declared in scope;
-
-#include <bits/stdc++.h>
-using namespace std;
-
-int fib(int n){
-	if(n<=1) return n;
-		if(dp[n]!= -1) return dp[n];
-	return dp[n] =fib(n-1) + fib (n-2);
-}
-
-int main(){
-
-int n ;
-cin >>n;
-int dp[n+1]; 
-memset(dp,-1,sizeof (dp));
-cout << fib(n);
-return 0;
-}
-
-
-
-#include <bits/stdc++.h>                 //## Approach
-using namespace std;
-
-int fib(int n, int dp[]){
-	if(n<=1) return n;
-		if(dp[n]!= -1) return dp[n];
-
-	return dp[n+1] = fib(n-1,dp) + fib(n-2,dp);
-}
-
-int main(){
-
-int n ;
-cin >> n;
-int dp[n+1]; 
-memset(dp,-1,sizeof(dp));
-cout << fib(n,dp);
-return 0;
-}
-   //      USING VECTOR
-
-
-#include <bits/stdc++.h>                
-using namespace std;
-
-int fib(int n, vector<int> &dp){
-	if(n<=1) return n;
-
-		if(dp[n]!= -1) return dp[n];
-
-	return dp[n] = fib(n-1, dp) + fib(n-2, dp);
-}
-
-int main(){
-
-int n ;
-cin >>n;
-
-vector<int> dp(n+1,-1);
-cout << fib(n,dp);
-return 0;
-}
-
-// T.C. = O(N)
-// S.C. = 0(N) + O(N) (Stack space recursion + array )
-
-
- //// ###
-
-
-
-#include <bits/stdc++.h>                
-using namespace std;
-
-int fib(int n, vector<int> &dp){
-	
-	dp[0]=0;
-	dp[1]=1;
-
- for (int i=2 ; i<= n; i++){
-	 dp[i] = dp[i-1] + dp[i-2];
-}
-return dp[n];
-}
-
-int main(){
-int n ;
-cin >>n;
-
-vector<int> dp(n+1,-1);
-	
-cout << fib(n,dp);
-return 0;
-}
-
-// T.C. = O(N)
-// S.C. = 0(N) ( array )
-
-
-#include <bits/stdc++.h>                
-using namespace std;
-
-    int fib(int n) {
-         if(n==0)return 0;
-         int prev2 =0 , prev = 1;
-            for(int i =2;i<=n;i++){
-            int curr_i = prev + prev2;
-            prev2 = prev;
-            prev =  curr_i;
-        }
-        return prev;
-    }
-
-int main(){
-int n ;
-cin >>n;
-	
-cout << fib(n);
-return 0;
-}
-
-// T.C. = O(N)
-// S.C. = 0(1)
-
-
-
-
-########################################################################################################################################################################################################################################################################################################################
-
-
-
-
-
 // LeetCode(509)
 
 
@@ -174,9 +9,11 @@ public:
         }
 
   }; 
-// T.C. = O(2^N) S.C.= O(N)   overlapping subproblems -> solution memorization 
 
+// T.C. = O(2^N) S.C.= O(N)   overlapping subproblems -> solution : memorization 
 
+class Solution {
+public:
     int f(int n, vector<int>&dp)
     {
         if(n<=1) return n;
@@ -188,9 +25,10 @@ public:
          return f(n,dp);
     }
 };
-//T.C. = O(N) S.C.= O(N)+ O(N)  recursive stack space + dp space -> solution  tabulation
+//T.C. = O(N) S.C.= O(N)+ O(N)  recursive stack space + dp space  -> solution : tabulation
 
-
+class Solution {
+public:
   int f(int n, vector<int>&dp) 
     {
       dp[0]=0 ; dp[1]=1;
@@ -204,9 +42,10 @@ public:
          return f(n,dp);
     }
 }; 
-// T.C. = O(N) S.C. = O(N)   dp space -> solution  space optimize 
+// T.C. = O(N) S.C. = O(N)   dp space -> solution : space optimize 
 
-
+class Solution {
+public:
     int fib(int n){
        int  prev2=0 , prev1=1;
        if(n==0) return 0;
@@ -220,4 +59,51 @@ public:
          return prev1;
     }
 };     
-// T.C. = O(N) S.C. = O(1)   dp space -> solution  space optimize 
+// T.C. = O(N) S.C. = O(1)  
+
+
+
+
+
+class Solution {
+public:
+    const int mod = 1e9 + 7;
+    vector<vector<int>> multiply(vector<vector<int>> a, vector<vector<int>> b) {
+
+        vector<vector<int>> c(2, vector<int>(2, 0));
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                for (int k = 0; k < 2; k++) {
+                    // c[i][j] = (c[i][j] + (long long)((long long)a[i][k] *
+                    // (long long)b[k][j])) % mod;
+                    c[i][j] = (c[i][j] + (long long)(a[i][k]) * b[k][j]) % mod;
+                }
+            }
+        }
+
+        return c;
+    }
+
+    vector<vector<int>> f(vector<vector<int>>& a, int n) {
+
+        if (n == 1)
+            return a;
+
+        vector<vector<int>> temp = f(a, n / 2);
+
+        if (n % 2 == 0)
+            return multiply(temp, temp);
+        return multiply(temp, multiply(temp, a));
+    }
+
+    int fib(int n) {
+        if (n == 0)
+            return 0;
+        vector<vector<int>> a = {{1, 1}, {1, 0}};
+        vector<vector<int>> ans = f(a, n);
+
+        return ans[1][0] % mod;
+    }
+};
+// T.C. = O(log n) S.C. = O(log n) 
