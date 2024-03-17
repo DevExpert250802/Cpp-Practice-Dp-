0e54f086879c14a147d567ba48c7818631ede805
Devansh Khandelwal <108886695+dev25082@users.noreply.github.com>
2024-03-17 11:28:52 +0530

    ClimbingStairs.cpp

--- 1D DP/ClimbingStairs.cpp
+++ 1D DP/ClimbingStairs.cpp
@@ -1,5 +1,5 @@
-/ * Q2)   Problem Statement: Given the number of stairs. Starting from the 0th stair we need to climb to the “Nth” stair.
- At a time we can climb either one or two steps. We need to return the total number of distinct ways to reach from 0th to Nth stair.*/
+/* Q2)   Problem Statement: Given the number of stairs. Starting from the 0th stair we need to climb to the “Nth” stair.
+ At a time we can climb either one or two steps. We need to return the total number of distinct ways to reach from 0th to Nth stair. */
 
 
 
@@ -73,4 +73,35 @@ public:
 }; 
 
 
-/* T.C. = O(N) S.C.= O(N)  recursive stack space  */
+/* T.C. = O(N) S.C.= O(1)    */
+
+
+
+class Solution {
+public:
+vector<vector<int>> multiply(const vector<vector<int>>& a, const vector<vector<int>>& b) {
+    vector<vector<int>> c(2, vector<int>(2, 0));
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            for (int k = 0; k < 2; k++) {
+                c[i][j] = (c[i][j] + (long long)(a[i][k]) * b[k][j]) ;
+            }
+        }
+    }
+    return c;
+}
+vector<vector<int>> f( vector<vector<int>>& a, int n) {
+    if (n == 1)return a;
+    vector<vector<int>> temp = f(a, n / 2);
+    if (n % 2 == 0)
+        return multiply(temp, temp);
+    return multiply(temp, multiply(temp, a));
+}
+int climbStairs(int n){
+    vector<vector<int>> a = {{1, 1}, {1, 0}};
+    vector<vector<int>> ans = f(a, n);
+    return ans[0][0];
+}
+};
+
+/* T.C. = O(logN) S.C.= O(logN)    */
