c93e6efdf1e41e81e0731079fd7bf8f79e5fc770
Devansh Khandelwal <108886695+dev25082@users.noreply.github.com>
2024-03-19 09:20:52 +0530

    FrogJump.cpp

--- 1D DP/FrogJump.cpp
+++ 1D DP/FrogJump.cpp
@@ -5,102 +5,70 @@ At a time the frog can climb either one or two steps. A height[N] array is also 
 
 #include<bits/stdc++.h>
 int f(int ind, vector<int> &heights) {
-  if (ind <=0)
-    return 0;
-  int left = f(ind - 1, heights) + abs(heights[ind] - heights[ind - 1]);
-  int right = INT_MAX;
-  if (ind > 1)
-    right = f(ind - 2, heights) + abs(heights[ind] - heights[ind - 2]);
-  return min(left, right);
+      if (ind ==0) return 0;
+      int left = f(ind - 1, heights) + abs(heights[ind] - heights[ind - 1]);
+      int right = INT_MAX;
+      if (ind > 1)right = f(ind - 2, heights) + abs(heights[ind] - heights[ind - 2]);
+       return min(left, right);
 }
-int frogJump(int n, vector<int> &heights)
-{
+int frogJump(int n, vector<int> &heights){
    return f(n-1,heights);
 }
 
 
-
-
-
+// T.C. = O(2^N) S.C.= O(N)   overlapping subproblems -> solution : memorization 
 
 
 #include<bits/stdc++.h>
 int f(int ind, vector<int> &heights,vector<int>&dp) {
-  if (ind <=0)
-    return 0;
-    if(dp[ind]!=-1) return dp[ind];
-  int left = f(ind- 1, heights,dp) + abs(heights[ind] - heights[ind - 1]);
-  int right = INT_MAX;
-  if (ind > 1)
-    right = f(ind - 2, heights,dp) + abs(heights[ind] - heights[ind - 2]);
- return dp[ind] = min(left, right);
+        if (ind ==0)return 0;
+        if(dp[ind] != -1) return dp[ind];
+        int left = f(ind - 1, heights,dp) + abs(heights[ind] - heights[ind - 1]);
+        int right = INT_MAX;
+        if (ind > 1)right = f(ind - 2, heights,dp) + abs(heights[ind] - heights[ind - 2]);
+        return dp[ind]=min(left, right);
 }
-int frogJump(int n, vector<int> &heights)
-{
-  vector<int>dp(n,-1);
-   return f(n-1,heights,dp);
+int frogJump(int n, vector<int> &heights){
+vector<int>dp(n,-1);
+  return f(n-1,heights,dp);
 }
 
 
+//T.C. = O(N) S.C.= O(N)+ O(N)  recursive stack space + dp space  -> solution : tabulation
 
 
 
 #include<bits/stdc++.h>
-int f(int n, vector<int> &heights,vector<int>&dp) {
-  dp[0]=0;
-  for (int i = 1; i <= n; i++) {
-    int left = dp[i-1] + abs(heights[i] - heights[i - 1]);
-    int right = INT_MAX;
-    if (i> 1)
-      right = dp[i-2] + abs(heights[i] - heights[i - 2]);
-    dp[i] = min(left, right);
-  }
-  return dp[n];
-}
-int frogJump(int n, vector<int> &heights)
-{
-  vector<int>dp(n,-1);
-   return f(n-1,heights,dp);
+int frogJump(int n, vector<int> &heights){
+   vector<int>dp(n,0);
+   dp[0]=0;
+   for (int i = 1; i <n; i++) {
+      int left = dp[i - 1] + abs(heights[i] - heights[i - 1]);
+      int right = INT_MAX;
+      if (i > 1) right = dp[i-2] + abs(heights[i] - heights[i - 2]);
+      dp[i] = min(left, right);
+    }
+   return dp[n-1];
 }
 
-
-
+// T.C. = O(N) S.C. = O(N)   dp space -> solution : space optimize 
 
 
 
 #include<bits/stdc++.h>
 
-int frogJump(int n, vector<int> &heights)
-{
-  vector<int>dp(n,-1);
-    dp[0]=0;
-  for (int i = 1; i < n; i++) {
-    int left = dp[i-1] + abs(heights[i]-heights[i-1]);
-    int right = INT_MAX;
-    if (i> 1)
-      right = dp[i-2] + abs(heights[i]-heights[i-2]);
-    dp[i] = min(left, right);
-  }
-  return dp[n-1];
-}
+int frogJump(int n, vector<int> &heights){
+    int prev2=0, prev=0;
+    for (int i = 1; i < n; i++) {
+        int left = prev + abs(heights[i]-heights[i-1]);
+        int right = INT_MAX;
+        if (i> 1) right = prev2 + abs(heights[i]-heights[i-2]);
+        int curr= min(left, right);
+        prev2=prev;
+        prev =curr;
+      }
+   return prev;
+ }
 
 
-
-
-
-#include<bits/stdc++.h>
-
-int frogJump(int n, vector<int> &heights)
-{
-    int prev=0, prev2=0;
-  for (int i = 1; i < n; i++) {
-    int left = prev + abs(heights[i]-heights[i-1]);
-    int right = INT_MAX;
-    if (i> 1)
-      right = prev2 + abs(heights[i]-heights[i-2]);
-   int curr= min(left, right);
-   prev2=prev;
-   prev =curr;
-  }
-  return prev;
-}
+// T.C. = O(N) S.C. = O(1)
