5828213d55395574ce24a12d0ead1b3a0ee7033c
Devansh Khandelwal <108886695+dev25082@users.noreply.github.com>
2024-04-03 15:44:48 +0530

    Partition Set Into 2 Subsets With Min Absolute Sum Diff.cpp

--- DP on Subsequences/Partition Set Into 2 Subsets With Min Absolute Sum Diff.cpp
+++ DP on Subsequences/Partition Set Into 2 Subsets With Min Absolute Sum Diff.cpp
@@ -2,7 +2,7 @@
 
 We need to return only the minimum absolute difference of the sum of elements of the two partitions. */
 
-
+Approach 1:
 
 
 
@@ -49,6 +49,13 @@ Space Complexity: O(N*totSum) + O(N)
 
 
 
+Approach 2:
+
+
+
+	
+
+
 
 int minSubsetSumDifference(vector<int>& arr, int n)
 {
@@ -87,14 +94,68 @@ Space Complexity: O(totSum)
 
 
 
-int minSubsetSumDifference(vector<int>& arr, int n)
-{ 
-        int totSum = 0;
 
+Optimize
+
+
+
+int minSubsetSumDifference(vector<int>& arr, int n) {
+    int totSum = accumulate(arr.begin(), arr.end(), 0);
+    int target = totSum / 2;
+
+    vector<vector<bool>> dp(n, vector<bool>(target + 1, false));
+    
+    dp[0][0] = true;
+    if (arr[0] <= target) {
+        dp[0][arr[0]] = true;
+    }
+
+    for (int i = 1; i < n; i++) {
+        for (int j = 1; j <= target; j++) {
+            dp[i][j] = dp[i - 1][j];
+            if (arr[i] <= j) {
+                dp[i][j] = dp[i][j] || dp[i - 1][j - arr[i]];
+            }
+        }
+    }
+
+    int mini = INT_MAX;
+    for (int j = 0; j <= target; j++) {
+        if (dp[n - 1][j]) {
+            int diff = abs(j - (totSum - j));
+            mini = min(mini, diff);
+        }
+    }
+
+    return mini;
+}
+
+
+
+
+Approach 3:
+
+
+
+
+Time Complexity: O(N*totSum/2) +O(N) +O(N)
+Space Complexity: O(totSum)
+
+
+
+
+
+
+
+
+
+
+int minSubsetSumDifference(vector<int>& arr, int n){ 
+    int totSum = 0;
   for (int i = 0; i < n; i++) {
     totSum += arr[i];
   }
-        vector < bool > prev(totSum + 1, false),cur(totSum + 1, false);
+vector < bool > prev(totSum + 1, false),cur(totSum + 1, false);
 
   prev[0] = true;
 
@@ -134,44 +195,40 @@ Space Complexity: O(N*totSum)
 
 
 
-
-
-
-
-int minSubsetSumDifference(vector<int>& arr, int n){
+#include<bits/stdc++.h>
+int minSubsetSumDifference(vector<int>& arr, int n) {
     int totSum = accumulate(arr.begin(), arr.end(), 0);
     int target = totSum / 2;
-        vector < bool > prev(totSum + 1, false),cur(totSum + 1, false);
 
-  prev[0] = true;
-
-  if (arr[0] <= totSum)
-    prev[arr[0]] = true;
-
-  for (int ind = 1; ind < n; ind++) {
-    cur[0] = true;
-    for (int target = 1; target <= totSum; target++) {
-      bool notTaken = prev[target];
-
-      bool taken = false;
-      if (arr[ind] <= target)
-        taken = prev[target - arr[ind]];
-
-      cur[target] = notTaken || taken;
+    vector<bool> prev(target + 1, false), cur(target + 1, false);
+    prev[0] = true;
+    if (arr[0] <= target) {
+        prev[arr[0]] = true;
     }
-    prev = cur;
-  }
 
-  int mini = 1e9;
-  for (int i = 0; i <= totSum/2; i++) {
-    if (prev[i] == true) {
-      int diff = abs(i - (totSum - i));
-      mini = min(mini, diff);
+    for (int ind = 1; ind < n; ind++) {
+        cur[0] = true;
+        for (int tar = 1; tar <= target; tar++) {
+            bool notTaken = prev[tar];
+            bool taken = false;
+            if (arr[ind] <= tar) {
+                taken = prev[tar - arr[ind]];
+            }
+            cur[tar] = notTaken || taken;
+        }
+        prev = cur;
     }
-  }
-  return mini;
+
+    int mini = INT_MAX;
+    for (int j = 0; j <= target; j++) {
+        if (prev[j]) {
+            int diff = abs(j - (totSum - j));
+            mini = min(mini, diff);
+        }
+    }
+
+    return mini;
 }
-
 
 
 Time Complexity: O(n * total sum/2) +O(N) +O(N)
