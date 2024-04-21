8d57a6d3c425d8d5ebf1b26726a9bd66d40da3c5
Devansh Khandelwal <108886695+dev25082@users.noreply.github.com>
2024-04-21 21:14:36 +0530

    hortest Common Supersequence  ( length )

---
+++ DP on Strings/7.cpp
@@ -0,0 +1,25 @@
+Shortest Common Supersequence  ( length )
+
+
+
+int lcs(string &s, string &t){
+   int n=s.size();
+   int m=t.size();
+    vector<int>prev(m+1,0),cur(m+1,0);
+	for(int i=1;i<=n;i++){
+	   for(int j=1;j<=m;j++){
+	      if(s[i-1]==t[j-1]) cur[j] =1 +prev[j-1];
+	      else cur[j]=max(prev[j],cur[j-1]);
+	   }
+           prev=cur;
+	}
+    return prev[m];		 
+   }
+
+int lengthofSupersequence(string &s1, string &s2){
+
+    int n  = s1.size();
+    int m  = s2.size();
+    return n + m -lcs(s1,s2);
+
+}
