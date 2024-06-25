00c7f18bcb765533a1062a76a757be1fc11fc54c
Devansh Khandelwal <108886695+DevExpert250802@users.noreply.github.com>
2024-06-25 16:25:25 +0530

    Students Happiness

--- Bitmask DP/01.cpp
+++ Bitmask DP/01.cpp
@@ -1,68 +1,136 @@
-/*                                                       Students Happiness
+/*              https://maang.in/problems/Students-Happiness-452?resourceUrl=cs74-cp487-pl3292-rs452
 
-Description :                                                
-There are N Ranks and M students in a class. Each student has a happiness value for the rank in the class he gets. The i th Student gets happy[i][j] on getting a rank j. 
-It's ok to not keep all students in the ranks (They get 0 happiness). At most one student can be assigned to a rank.
+                                                       Students Happiness
+Description :
+There are N Ranks and M students in a class. Each student has a happiness value for the rank in the class he gets. The i th Student gets happy[i][j] on getting a rank j.
+It's ok to not keep all students in the ranks (They get 0 happiness). At most one student can be assigned to a rank. 
 The total happiness of the assignment can be calculated as Sum of Happiness of each of the students in the class. Find the best way to calculate.
 
+Input Format:
+The first line contains an integer T (1 ≤ T ≤ 200) - the number of testcases.
 The first line of each test case contains two space-separated integers N, M. 1 ≤ N ≤ 10, 1 ≤ M ≤ 50.
-
 The next M lines contain N integers, 0 ≤ happy[i][j] ≤ 10^5.
 
+Output Format:
+For each test case print the maximum sum of happiness possible in a new line.
 
 
-  */
+Constraints
+Sample Input 1
+4
+6 11
+49 78 23 100 29 2
+11 28 39 27 77 45
+24 52 18 45 84 41
+85 88 75 21 98 100
+59 24 27 21 94 90
+88 0 0 16 89 54
+63 24 100 23 18 75
+26 34 54 14 28 11
+68 61 40 14 67 14
+16 62 17 70 29 52
+97 53 0 98 90 75 
+1 2
+8
+6 
+5 9
+6 0 7 9 3
+10 0 1 1 4
+0 2 4 6 1
+7 1 5 5 10
+0 3 3 1 10
+9 8 6 9 1
+0 10 6 0 2
+4 9 2 4 9
+4 10 10 3 7 
+4 5
+10 2 4 0
+10 1 6 7
+6 3 2 0
+2 3 5 1
+10 4 7 6 
+
+Sample Output 1 :
+564
+8
+49
+27
+
+ */
+
+
+
+
+
+/*
+TestCase  explanation :
+4 5             n = 4 (ranks)    m=5 (students)
+10 2 4 0        => 10 (1st)
+10 1 6 7        =>  7 (4th)
+6 3 2 0         =>  3 (2nd)
+2 3 5 1
+10 4 7 6        => 7  (3rd)
+
+10+7+7+3= 27
+
+
+
+5 9
+6 0 7 9 3       
+10 0 1 1 4        => 10 (1st)
+0 2 4 6 1
+7 1 5 5 10        => 10 (5th)
+0 3 3 1 10
+9 8 6 9 1         => 9  (4th)
+0 10 6 0 2        => 10 (2nd)
+4 9 2 4 9
+4 10 10 3 7       => 10 (3rd)
+
+10+10+9+10+10  = 49
+
+*/
 
 
 
 
 #include <bits/stdc++.h>
 using namespace std;
- 
-#define int long long
- 
-int happy[50][10];
-int dp[50][(1<<10)];
-int n, m;
- 
-int dfs(int pos, int vis) {
-    if(pos == m) {
-        return 0;
-    }
-    if(dp[pos][vis] != -1) {
-        return dp[pos][vis];
-    }
-    int ans = dfs(pos+1, vis);
-    for (int i = 0; i < n; ++i) {
-        if((vis & (1<<i)) == 0) {
-            int temp = happy[pos][i] + dfs(pos+1, vis | (1<<i));
-            ans = max(ans, temp);
+
+int f(int i, int mask, int n, int m,  vector<vector<int>>& happy, vector<vector<int>>& dp) {
+    if (i == m) return 0;
+    if (dp[i][mask] != -1) return dp[i][mask];
+
+    int ans = f(i + 1, mask, n, m, happy, dp);
+    for (int j = 0; j<n; j++) {
+        
+        if ((mask & (1 << j)) == 0) {
+            ans = max(ans,  happy[i][j] + f(i+1, mask | (1 << j), n, m,happy,dp ));
         }
     }
-    dp[pos][vis] = ans;
-    return ans;
+    return dp[i][mask] = ans;
 }
- 
-signed main(){
+
+signed main() {
     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-    int t;
-    cin >> t;
-    while(t--) {
+
+    int T;
+    cin >> T;
+    while (T--) {
+        int n, m;
         cin >> n >> m;
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < (1<<n); ++j) {
-                dp[i][j] = -1;
-            }
-        }
+        vector<vector<int>> happy(m, vector<int>(n));
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
                 cin >> happy[i][j];
             }
         }
-        cout << dfs(0, 0) << "\n";
+
+        vector<vector<int>> dp(m, vector<int>(1 << n, -1));
+        cout << f(0, 0, n, m, happy, dp) << endl;
     }
     return 0;
 }
+
 
 
 
