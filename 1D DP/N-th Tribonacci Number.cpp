125089561627642788fbb2d2ef30675ed05232c6
Devansh Khandelwal <108886695+DevExpert250802@users.noreply.github.com>
2025-03-17 20:21:08 +0530

    Update N-th Tribonacci Number.cpp

--- 1D DP/N-th Tribonacci Number.cpp
+++ 1D DP/N-th Tribonacci Number.cpp
@@ -64,6 +64,8 @@ class Solution {
 public:
     int tribonacci(int n) {
         if (n == 0) return 0;
+        if (n == 1 || n == 2) return 1;
+
         int prev3 = 0, prev2 = 1, prev1 = 1;
         for (int i = 3; i <= n; i++) {
             int curr = prev3 + prev2 + prev1;
@@ -74,5 +76,6 @@ public:
         return prev1;
     }
 };
+
 // Time Complexity: O(N)  
 // Space Complexity: O(1)  
