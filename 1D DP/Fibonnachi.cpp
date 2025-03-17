de38e286b7b6dabbcea4b7c3894f0b357d3385a9
Devansh Khandelwal <108886695+DevExpert250802@users.noreply.github.com>
2025-03-17 11:30:31 +0530

    Update Fibonnachi.cpp

--- 1D DP/Fibonnachi.cpp
+++ 1D DP/Fibonnachi.cpp
@@ -30,19 +30,17 @@ public:
 
 class Solution {
 public:
-  int f(int n, vector<int>&dp) 
-    {
-      dp[0]=0 ; dp[1]=1;
-      for(int i=2; i<=n; i++){
-          dp[i]=f(i-1,dp)+f(i-2,dp);
-          }
-          return dp[n];
+    int fib(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        vector<int> dp(n + 1);
+        dp[0] = 0, dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];  // Using previously computed values
+        }
+        return dp[n];
     }
-    int fib(int n){
-         vector<int>dp(n+1,-1);
-         return f(n,dp);
-    }
-}; 
+};
 // T.C. = O(N) S.C. = O(N)   dp space -> solution : space optimize 
 
 
